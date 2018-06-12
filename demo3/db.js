81f50d905c0cbeca347fe369d72ef5cfb9e89687
u51 <u51@u51deMacBook-Pro.local>
2018-06-12 18:56:04 +0800

    修复数据存储位置bug

--- demo3/db.js
+++ demo3/db.js
@@ -1,6 +1,7 @@
 const levelup = require('levelup')
 const leveldown = require('leveldown')
-
+const path = require('path');
+const dbPath = path.join(__dirname, './mydb')
 // 1) Create our store
-const db = levelup(leveldown('./mydb'))
+const db = levelup(leveldown(dbPath))
 module.exports = db;
