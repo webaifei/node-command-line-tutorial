3fb724862026b7fd6d396d1f39f1c7f2bf7ec8e1
ngnice <ngnice@ngnicedeMacBook-Pro.local>
2019-12-26 10:02:40 +0800

    updage version

--- fnpm/index.js
+++ fnpm/index.js
@@ -10,7 +10,7 @@ var devDeps = packageContent.devDependencies;
 var deps = packageContent.dependencies
 var packgeFile = 'package.json';
 var packgeBakFile = 'package.json.bak';
-shell.exec('==========开始处理package.json文件=========');
+shell.echo('==========开始处理package.json文件=========');
 // step3 删除@u51 依赖
 Object.keys(devDeps).forEach(function (dep) {
     if(/^@u51/.test(dep)) {
@@ -23,20 +23,20 @@ Object.keys(deps).forEach(function (dep) {
         delete deps[dep];
     }
 })
-shell.exec('==========备份package.json文件=========');
+shell.echo('==========备份package.json文件=========');
 // step2 备份
 fs.copyFileSync(path.join(workdir, packgeFile), path.join(workdir, packgeBakFile));
-shell.exec('==========生成新的package.json文件=========');
+shell.echo('==========生成新的package.json文件=========');
 // step4 生成新的package.json
 fs.writeFileSync(path.join(workdir, packgeFile), JSON.stringify(packageContent), 'utf-8');
 
-shell.exec('==========安装非u51依赖=========');
+shell.echo('==========安装非u51依赖=========');
 // step5 执行npm i --registry=http://npm.u51-inc.com/
 if(shell.exec('npm i') == 0) {
     // step6 恢复package.json 执行 npm i
-    shell.exec('==========添加u51依赖=========');
-    fs.copyFileSync(path.join(workdir, packgeBakFile), path.join(workdir, packgeFile));
-    shell.exec('==========安装u51依赖=========');
+    shell.echo('==========添加u51依赖=========');
+    shell.exec(`mv ${path.join(workdir, packgeBakFile)}, ${path.join(workdir, packgeFile)}`);
+    shell.echo('==========安装u51依赖=========');
     if(shell.exec('npm i --registry=http://npm.u51-inc.com/') == 0) {
         shell.echo('Nice! Everything is ok!');
     }
